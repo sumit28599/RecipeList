ef03f1015fb7d9f458a3af36e6b04a258d8c0452
Priya Jaiswal <pj7167764@gmail.com>
2025-01-23 19:44:20 +0530

    Initial Commit

---
+++ RecipeList AppTests/RecipeList_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeList_AppTests.swift
+//  RecipeList AppTests
+//
+//  Created by Mac on 23/01/25.
+//
+
+import Testing
+@testable import RecipeList_App
+
+struct RecipeList_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
